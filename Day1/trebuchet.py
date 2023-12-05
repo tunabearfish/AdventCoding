567668ed2bfa8e008f55d63bdcb827805178ac18
Steven Nguyen <omgsteven@gmail.com>
2023-12-05 10:09:35 -0500

    creating new repo for adventCode challenges

---
+++ Day1/trebuchet.py
@@ -0,0 +1,21 @@
+file = 'input.txt'
+
+try:
+    with open(file, 'r') as f:
+        lines = f.readlines()
+
+        total = 0
+
+        for line in lines:
+            digits = [char for char in line if char.isdigit()]
+
+            if digits:
+                sum = int(digits[0] + digits[-1])
+                total += sum
+        print("total sum is ",total)
+except FileNotFoundError:
+    print('File not found!')
+except Exception as e:
+    print(f"an error occured:{e}")
+
+
